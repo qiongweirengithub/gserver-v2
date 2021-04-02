541ca26f51818cfaf8449ef05557050292fecb7d
qiongwei_ren <qiongwei_ren@163.com>
2021-04-02 23:39:43 +0800

    add gbattleroomsvc

---
+++ bin/conf/battle-release/battle.Dockerfile
@@ -0,0 +1,26 @@
+# Docker image for springboot file run
+# VERSION 0.0.1
+# Author: eangulee
+# 基础镜像使用java
+FROM alpine:3.5
+# 作者
+MAINTAINER dika <dika@gmail.com>
+# VOLUME 指定了临时文件目录为/tmp。
+# 其效果是在主机 /var/lib/docker 目录下创建了一个临时文件，并链接到容器的/tmp
+VOLUME /tmp 
+
+WORKDIR ～
+
+# 将jar包添加到容器中并更名为app.jar
+ADD gserverv2 /usr/bin/gserverv2 
+ADD server.json server.json
+ADD env.json env.json
+
+# 运行jar包
+# RUN bash -c 'touch /app.jar'
+
+#ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+
+ENTRYPOINT ["gserverv2","-conf","server.json","-pid", "battleroomsvc"]
+# ENTRYPOINT ["gserverv2","-consul_addr", "172.17.0.1:8500", "-nats_addr", "nats://172.17.0.1:4222", "-conf","server.json"]
+
---
+++ deploy/application/g-battleroomsvc-deploy.go
@@ -0,0 +1,60 @@
+package application
+
+import (
+	"fmt"
+)
+
+
+func DeployingGBattleRoomsvc(wd string, service string, roomid string)  (string, string, error) {
+
+	// check param
+	// 1. port 是否被占用
+
+	fmt.Println("deploying battle room service")
+
+	_, err := ExecCICmd(wd, "cp", "./bin/conf/battle-release/server.json", "./")
+	if err != nil {
+		fmt.Print(err)
+		return "", "", err
+	}
+
+	_, err = ExecCICmd(wd, "sed", "-i","s/ROOMID/" + roomid + "/g", "./server.json")
+	if err != nil {
+		fmt.Print(err)
+		return "","", err
+	}
+
+
+	_, err = ExecCICmd(wd, "cp", "./bin/conf/battle-release/battle.Dockerfile", wd)
+	if err != nil {
+		fmt.Print(err)
+		return "","", err
+	}
+
+	_, err = ExecCDCmd(wd, "docker", "build", "-t", "g-battle-room", "-f", wd + "/battle.Dockerfile", ".")
+	if err != nil {
+		fmt.Print(err)
+		return "","", err
+	}
+	fmt.Println("battle docker build succ")
+
+	appId := "-" + RandString(6)
+	containerName := service + appId
+	containerPidFileName := service+appId+".pid"
+
+	_, err = ExecCDCmd(wd, "docker","run","-d","--name", containerName, "--cidfile" , containerPidFileName, "g-battle-roomsvc")
+	if err != nil {
+		fmt.Print(err)
+		return "","", err
+	}
+
+	fmt.Println("new battle room service: ", roomid)
+
+	pdiFile := wd + "/" + containerPidFileName
+	pid := GetPid(pdiFile)
+	fmt.Println("pid file:", pdiFile, ", pid: ", pid)
+
+	// record to db
+	return pid, containerName, nil
+
+}

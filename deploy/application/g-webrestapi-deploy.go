fea4e514f7fbdfdf96598fa17c9128f7bb76c8bf
qiongwei_ren <qiongwei_ren@163.com>
2021-04-10 23:12:52 +0800

    push all

--- deploy/application/g-webrestapi-deploy.go
+++ deploy/application/g-webrestapi-deploy.go
@@ -10,33 +10,34 @@ func DeployingGWebRestApi(wd string, service string, port string)  (string, stri
 	// check param
 	// 1. port 是否被占用
 
-	fmt.Println("deploying web")
+	fmt.Println(MagentaBg, service, "preparing docker env", Reset)
 
 	_, err := ExecCICmd(wd, "cp", "./bin/conf/web-release/server.json", "./")
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(Red, "copying server.json error for web rest api svc ", err, Reset)
 		return "", "", err
 	}
 
 	_, err = ExecCICmd(wd, "sed", "-i","s/PORT/" + port + "/g", "./server.json")
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(Red, "replace roomid for web rest api errr ", err, Reset)
 		return "","", err
 	}
 
 
 	_, err = ExecCICmd(wd, "cp", "./bin/conf/web-release/web.Dockerfile", wd)
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(Red, "copying battle.Dockerfile error for web rest api ", err, Reset)
 		return "","", err
 	}
 
 	_, err = ExecCDCmd(wd, "docker", "build", "-t", service, "-f", wd + "/web.Dockerfile", ".")
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(Red, "building image error for web rest api ", err, Reset)
 		return "","", err
 	}
-	fmt.Println("web docker build succ")
+
+	fmt.Println(MagentaBg, service, "starting in docker", Reset)
 
 	appId := "-" + RandString(6)
 	containerName := service + appId
@@ -44,15 +45,16 @@ func DeployingGWebRestApi(wd string, service string, port string)  (string, stri
 
 	_, err = ExecCDCmd(wd, "docker","run","-d","--name", containerName, "--cidfile" , containerPidFileName,"-p"+port+":"+port, service)
 	if err != nil {
-		fmt.Print(err)
+		fmt.Println(Red, "running docker error for web rest api ", err, Reset)
 		return "","", err
 	}
 
-	fmt.Println("runing with ports: ", port)
+	fmt.Println(MagentaBg, service, "started success in docker", Reset)
 
-	pdiFile := wd + "/" + containerPidFileName
-	pid := GetPid(pdiFile)
-	fmt.Println("pid file:", pdiFile, ", pid: ", pid)
+	pidFile := wd + "/" + containerPidFileName
+	pid := GetPid(pidFile)
+	fmt.Println(Yellow, "--->   pid:    ", pid,  Reset)
+	fmt.Println(Yellow, "--->   pidfile:", pidFile,  Reset)
 
 	// record to db
 	return pid, containerName, nil

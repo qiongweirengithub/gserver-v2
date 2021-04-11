bf13980a04c6e94c6848c35a1d4d8660a55949a6
qiongwei_ren <qiongwei_ren@163.com>
2021-04-11 23:30:37 +0800

    优化项目clone

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@
 #  静态编译
 CGO_ENABLED=0 go build -a -o gserverv2 -ldflags '-extldflags "-static"' .
 
- echo "编译完成"
+echo "编译完成"
 
- cp gserverv2 ~/myapp/k8s/app/
- cp ./bin/conf/env.json ~/myapp/k8s/app/
- cp ./bin/conf/server.json ~/myapp/k8s/app/
+#  cp gserverv2 ~/myapp/k8s/app/
+#  cp ./bin/conf/env.json ~/myapp/k8s/app/
+#  cp ./bin/conf/server.json ~/myapp/k8s/app/
